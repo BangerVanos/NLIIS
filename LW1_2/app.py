c69929a2334d22e7e84e851bb31f2728cb370e00
unknown <romchik349@gmail.com>
2024-09-25 22:43:27 +0300

    LW1 code done

--- LW1_2/app.py
+++ LW1_2/app.py
@@ -1,38 +1,62 @@
 import streamlit as st
 import streamlit_shadcn_ui as ui
 from backend.ex_processor import find_in_dir
+from backend.processor import Processor
+from backend.processor import validate_query, summarize_text
+from streamlit_extras.stateful_button import button
+from io import StringIO
+import os
+from random import randint
 
 
 def main():    
     st.set_page_config(page_title='Логический поиск в файлах', 
                        layout='wide')
     st.header('1. Введите путь к папке с документами')
-    docs =st.text_input(label='Путь к папке', key='docs_path')
+    docs = st.file_uploader(label='Путь к конфигурационному файлу', key='docs_path', type='txt',
+                            help='Данный документ формата .txt должен содержать пути к папкам с документами на устройствах сети')
     st.header('2. Введите логическое выражение')
     with st.expander('Пояснение к синтаксису логических запросов'):
         st.text(
             """Вложенность логического запроса регулируется скобками (т.е. для нового уровня вложенности подвыражение обрамляется круглыми скобками)
-            Поддерживаемые логические операторы:
-            AND - логическое И
-            OR - логическое ИЛИ
-            NOT - логическое НЕ
-            Выражения для поиска обрамляются одинарными кавычками (например, 'Текст')"""
+            Поддерживаемые союзы и частицы: 'и', 'или', 'не', 'но не', 'а также'
+            Выражения для поиска обрамляются одинарными кавычками (например, 'Текст').
+            После введения запроса его можно проверить на правильность, нажав на кнопку 'Проверить запрос на правильность'.
+            При нажатии на эту кнопку интеллектуальный агент проверит правильность синтаксиса вашего запроса.
+            Пример правильного запроса: 'Анна' или 'Каренина'"""
         )
     query = st.text_input(label='Запрос', key='query')
+    if query:
+        ask_btn = st.button(label='Проверить запрос на правильность', key='ask_btn')
+        if ask_btn:
+            validate_answer = validate_query(query)
+            st.info(validate_answer)
     st.header('3. Провести поиск по документам')
-    btn = st.button('Провести поиск', type='secondary', key='btn')
+    btn = button('Провести поиск', type='secondary', key='btn')
     metrics_chk = st.checkbox(label='Показать метрики?', key='metrics_chk')
     if btn:
         if not docs:
-            st.error('Укажите путь к папке с документами!')
+            st.error('Укажите путь к конфигурационному файлу!')
         if not query:
             st.error('Введите поисковое выражение!')
         else:
-            result = find_in_dir(query, docs)
+            dbs: list[str] = StringIO(docs.getvalue().decode('utf-8')).read().split('\n')
+            dbs = [folder.replace('\n', '').replace('\r', '').replace(os.sep, '/') for folder in dbs]            
+            proc = Processor(dbs, query)
+            result = proc.execute_query()
             st.session_state['result'] = result           
             st.write('## Релевантные документы:')
+                        
             for doc in result['documents']:
-                st.write(f'#### {doc}')            
+                with st.expander(label=doc):                
+
+                    st.write('Гиперссылка на документ')                
+                    st.write(f'<p><a href=file://localhost/{doc}>{doc}</a></p>', unsafe_allow_html=True)                    
+                    sum_btn = st.button(f'Привести краткое содержание текста документа', key=f'sum_{doc}')                                    
+
+                    if sum_btn:
+                        text_sum = summarize_text(doc)
+                        st.info(text_sum)       
             if metrics_chk:
                 st.write('## Метрики:')
                 text = (
@@ -42,7 +66,7 @@ def main():
                     f'F1-score: {result['metrics']['f1']}'
                 )                
                 st.write(text)
-    
+
 
 if __name__ == "__main__":
     # (('Здравствуйте' AND 'легенда') OR (NOT 'пора'))
--- LW1_2/backend/processor.py
+++ LW1_2/backend/processor.py
@@ -1,12 +1,142 @@
-from pathlib import Path
+from glob import glob
+from itertools import chain
+import re
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+load_dotenv()
 
 
 class Processor:
 
-    def __init__(self, db_folders: list[str], query: str) -> None:
-        self._files = None
-        self._query = query
-    
-    
+    PUNCTS: list[str] = [',', '.', '?', ':', '-', ';', '!', '...', '\n', '\r']
+    LOGICAL_OPERATIONS_MAP: dict[str, str] = {        
+        'или': 'or',
+        'но не': 'and not',        
+        'не': 'not',
+        'а также': 'and',        
+        'и': 'and',
+        'and': 'and',
+        'or': 'or',
+        'not': 'not',
+        '&': 'and',
+        '|': 'or',
+        '~': 'not',
+        '!': 'not'
+    }
 
+    def __init__(self, db_folders: list[str], query: str) -> None:
+        # for folder in db_folders:
+        #     print(glob(f'{folder.replace('\n', '').replace('\r', '').replace(os.sep, '/')}/*.txt', recursive=True))             
+        #     print(folder.replace('\n', '').replace('\r', '').replace(os.sep, '/'))
+        #     print('---------------------------')    
+        # path = 'D:\Programs\Programming\BSUIR\NLIIS\LW1_2\backend\file_db'
+        # if os.path.exists(path):
+        #     print(os.listdir(path))
+        # else:
+        #     print(f"The path {path} does not exist.")
+        # for file_path in db_folders:
+        #     print(os.listdir(os.path.normpath(file_path)))          
+        self._files = list(chain.from_iterable([glob(fr'{os.path.normpath(folder)}/*.txt', recursive=True)
+                                                for folder in db_folders]))           
+        print(self._files)
+        self._query = query
+        for sym in self.PUNCTS:
+            self._query = self._query.replace(sym, '')
+        self._query_tokens = list(map(lambda s: s.replace('\'', '').replace('"', '').lower(),
+                                      re.findall(re.compile(r'\'\w+\'|\"\w+\"'), self._query)))
+        raw_tokens = list(re.findall(re.compile(r'\'\w+\'|\"\w+\"'), self._query))
+        for token in raw_tokens:
+            self._query = self._query.replace(token, '[TAG]')
+        for operation, python_operation in self.LOGICAL_OPERATIONS_MAP.items():
+            self._query = self._query.replace(operation, python_operation)
+        for token in self._query_tokens:
+            self._query = self._query.replace('[TAG]', token, 1)
+        # print(self._query_tokens)
+        # print(self._query)
     
+    def execute_query(self) -> dict:        
+        result = []
+        for path in self._files:
+            query = self._query
+            with open(path, encoding='utf-8') as file:
+                text = file.read()
+            text = text.lower()            
+            for sym in self.PUNCTS:
+                text = text.replace(sym, '')
+            text_tokens = text.split(' ')
+            for token in self._query_tokens:
+                if token in text_tokens:
+                    query = query.replace(token, 'True', 1)
+                else:
+                    query = query.replace(token, 'False', 1)
+            # print(query)                       
+            if eval(query):
+                result.append(path)
+        return {
+            'documents': result,
+            'metrics': {
+                'acc': round(1, 2),
+                'prec': round(1, 2),
+                'rec': round(1, 2),
+                'f1': round(1, 2)
+            }
+        }
+
+
+def validate_query(query: str) -> str:
+    client = OpenAI(
+        api_key=os.getenv('API_KEY'), # ваш ключ в VseGPT после регистрации
+        base_url="https://api.vsegpt.ru/v1",
+    )
+    system_prompt = """Ты оцениваешь текстовые запросы, которые тебе поступают. Запросы выполнены на русском языке.
+    Запросы могут содержать следующие знаки пунктуации: ',', '.', '?', ':', '-', ';', '!', '...'.
+    Запросы содержат различные союзы, как, например: 'и', 'или', 'не', 'но не' и другие.
+    Запросы содержат ключевые слова, связанные этими союзами. Ключевое слово обязано быть обёрнуто в одиарные или 
+    двойные кавычки, например 'тест' или 'Тест'.
+    Примеры:
+    Query: 'Каренина' или 'Анна'
+    Answer: 'Это правильный запрос'
+    Query: 'Рогожин' и 'Мышкин', но не 'Анна'
+    Answer: 'Это правильный запрос'
+    Query: 'Анна и 'Каренина'
+    Answer: 'Это неправильный запрос, так как ключевое слово 'Анна' не обёрнуто в кавычки'
+    Query: 'Рогожин' и
+    Answer: 'Это неправильный запрос, так как союз 'и' должен связывать два ключевых слова'"""
+
+    response_big = client.chat.completions.create(
+        model="openai/gpt-4o-mini",
+        messages=[{'role': 'system', 'content': system_prompt}, {'role': 'user', 'content': query}],
+        temperature=1,
+        n=1,
+        max_tokens=3000, # максимальное число ВЫХОДНЫХ токенов. Для большинства моделей не должно превышать 4096
+        extra_headers={"X-Title": "My App" }, # опционально - передача информация об источнике API-вызова
+    )
+    response = response_big.choices[0].message.content
+    return response
+
+
+def summarize_text(path: str) -> str:
+    client = OpenAI(
+        api_key=os.getenv('API_KEY'), # ваш ключ в VseGPT после регистрации
+        base_url="https://api.vsegpt.ru/v1",
+    )
+    system_prompt = """Ты делаешь краткое содержание текста. Краткое содержание должно помещаться в 150 слов, не больше."""
+    with open(path, encoding='utf-8') as file:
+        text = file.read()
+    if len(text) < 50_000:
+        begin = 0
+    else:
+        begin = len(text) // 2
+    response_big = client.chat.completions.create(
+        model="openai/gpt-4o-mini",
+        messages=[{'role': 'system', 'content': system_prompt}, {'role': 'user', 'content': text[begin - 25000:begin + 25000]}],
+        temperature=1,
+        n=1,
+        max_tokens=3000, # максимальное число ВЫХОДНЫХ токенов. Для большинства моделей не должно превышать 4096
+        extra_headers={"X-Title": "My App" }, # опционально - передача информация об источнике API-вызова
+    )
+    response = response_big.choices[0].message.content
+    return response
